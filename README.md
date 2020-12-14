bcf63906ab8fff372325f63c482e3a79444ce4e2
lleclair <55537694+lleclair@users.noreply.github.com>
2020-12-14 19:10:56 +0100

    code clean and finished readme

--- README.md
+++ README.md
@@ -4,22 +4,19 @@ __`Team`__: arbitrageurs
 
 __`Team Members`__: Qais Humeid, Kim Ki Beom, Louis Leclair
 
-Before starting our project you must have the following libraries installed on your computer:
-
-run the ```pip install -r requirements.txt``` command to have all the following library install
+After downloading the git.
+To start our project you must have the following libraries installed on your computer:
 
 1. Numpy
-2. pytorch
-3. matplotlib
-4. pandas
-5. xgboost
-6. sklearn
+2. matplotlib
+3. pandas
+4. xgboost
+5. [pytorch](https://pytorch.org) (see install pytorch section)
+6. [sklearn](https://scikit-learn.org/stable/install.html)
 
-- Run the following script
-- download the git 
-- run python script `run.py`
+To install them, run the ```pip install -r requirements.txt``` command to have Numpym matplotlib, pandas and xgboost installed and for pytorch and sklearn you have to click on the link above and follow the instruction on the different websites.
 
-All the details are in the jupyter notebook where you can find all our plots, our data manipulations and etc...
+All the details are in the jupyter notebook file ``project2.ipynb``where you can find all our plots, our data manipulations, our trials and results. The code inside is comment to help you understand our thoughts and reflexion.
 
 ## Structure of the project 
 ```
@@ -49,6 +46,21 @@ Contains some helper functions to ease the comprehension and the density of the 
 - __`MAE`__: Compute the mean absolute error of 2 tensors.
 
 #### `Cross Validation functions`
-Helpers functions  __`build_k_indices`__, __`cross_validation`__. Which help us to find the best input and create some randomness to better training and testing.
+Helpers functions  __`build_k_indices`__, __`cross_validation`__. Which help us to find the best input and create some randomness to better training and testing. As well as __`cross_correlation_regression`__ and __`cross_correlation_boost`__ which are functions used to do linear regression with different models and loss functions.
 
+#### `Data Analysis`
+We just have the function __`show_heatmap`__ which helped us to find the correlation between each features of the given dataset.
+
+#### `Neural Network`
+
+We only have the __`train`__ method inside this part which is almost the same function as the one define in lab10 of this course with some minor modifications.
+
+### `Project2.ipynd`
+
+All our thoughts and reflexions are inside this file. The plan of the notebook is the following:
+
+- Import
+- Data analysis of a sample of size `threshold` with plots.
+- Linear regression part with cross correlation to have different results. The tested models are in this order `XGBoost`, `AdaBoost`, `Lasso`, `Linear Regression` the basic one, `SGDRegressor`.
+- Neural Network part, where we have a `CNN` and a `Fully Connected Neural Network`.
 
--- src/helper.py
+++ src/helper.py
@@ -1,15 +1,42 @@
 import numpy as np
 import sklearn
+import matplotlib.pyplot as plt
+
+# ==============================================
+# Cost functions
+# ==============================================
 
 def MSE(prediction, target):
+    """Compute the mean square error, adapt for tensors.
+
+    Args:
+        prediction (tensor): predicted data
+        target (tensor): target data
+
+    Returns:
+        float: the MSE between the prediction and target data.
+    """
     pred = prediction.detach().numpy()
     tar = target.numpy()
     return sklearn.metrics.mean_squared_error(y_true=tar, y_pred=pred)
 
 def MAE(prediction, target):
+    """Compute the mean absolute error, adapt for tensors
+
+    Args:
+        prediction (tensor): predicted data
+        target (tensor): target data
+
+    Returns:
+        float: the MAE between the prediction and target data. 
+    """
     pred = prediction.detach().numpy()
     tar = target.numpy()
     return sklearn.metrics.mean_absolute_error(y_true=tar, y_pred=pred)
+
+# ==============================================
+# Cross Validation
+# ==============================================
 
 # Cross validation
 def build_k_indices(y, k_fold, seed):
@@ -22,7 +49,17 @@ def build_k_indices(y, k_fold, seed):
     return np.array(k_indices)
 
 def cross_validation(y, x, k_indices, k):
-    """return the loss of ridge regression."""
+    """Create a random division of the data between training and test set depending of a list of random indices and an index.
+
+    Args:
+        y (np.array): Nx1 array of the target data.
+        x (np.array): NxD array of the feature data.
+        k_indices (np.array): MxP array of random indices used to divide the data in test and training set.
+        k (int): k index where to choose the random array of indices in k_indices.
+
+    Returns:
+        tuple: division of the data in training and test set.
+    """
     # get k'th subgroup in test, others in train
     te_indice = k_indices[k]
     tr_indice = k_indices[~(np.arange(k_indices.shape[0]) == k)]
@@ -33,3 +70,109 @@ def cross_validation(y, x, k_indices, k):
     x_tr = x[tr_indice]
     
     return x_tr, x_te, y_tr, y_te
+
+def cross_correlation_regression(y, x, k_indices, k_fold, f, f_error):
+    """Compute and print the loss for different k depending of the number of k_fold for different training and test set.
+    Show the coefficients of the weights if use the library sklearn.linear_model.
+
+    Args: 
+        y (np.array): Nx1 array of the target data.
+        x (np.array): NxD array of the feature data.
+        k_indices (np.array): MxP array of random indices used to divide the data in test and training set.
+        k_fold (int): number of set, we are going to divide our dataset.
+        f (function): function we use to do linear regression.
+        f_error (function): error function use to compute the loss between our predicted and target data.
+    """
+    for k in range(k_fold): 
+        x_tr, x_te, y_tr, y_te = cross_validation(y, x, k_indices, k)
+        regr = f
+        regr.fit(x_tr, y_tr)
+        pred = regr.predict(x_te)
+        print("k = {} and loss= {:.6f} and coef= {}".format(k, f_error(y_true=y_te, y_pred=pred), np.round(regr.coef_, 4)))
+
+def cross_correlation_boost(y, x, k_indices, k_fold, f, f_error):
+    """Compute and print the loss for different k depending of the number of k_fold for different training and test set.
+    Almost the same method as cross_correlation_regression but cannot print the coefficients of the weights, to use with AdaBoost or XGBoost.
+
+    Args:
+        y (np.array): Nx1 array of the target data.
+        x (np.array): NxD array of the feature data.
+        k_indices (np.array): MxP array of random indices used to divide the data in test and training set.
+        k_fold (int): number of set, we are going to divide our dataset.
+        f (function): function we use to do linear regression.
+        f_error (function): error function use to compute the loss between our predicted and target data.
+    """
+    for k in range(k_fold):
+        x_tr, x_te, y_tr, y_te = cross_validation(y, x, k_indices, k)
+        regr = f
+        regr.fit(x_tr, y_tr)
+        pred = regr.predict(x_te)
+        print("k = {} and Loss= {:.6f}".format(k, f_error(y_true=y_te, y_pred=pred)))
+
+# ==============================================
+# Data analysis
+# ==============================================
+
+def show_heatmap(data):
+    """Show the correlation map between the each row of the given dataset."""
+    plt.matshow(data.corr())
+    plt.xticks(range(data.shape[1]), data.columns, fontsize=14, rotation=90)
+    plt.gca().xaxis.tick_bottom()
+    plt.yticks(range(data.shape[1]), data.columns, fontsize=14)
+
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=14)
+    plt.title("Feature Correlation Heatmap", fontsize=14)
+    plt.show()
+
+# ==============================================
+# Neural network
+# ==============================================
+
+def train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs, f, device):
+    """Training method for our neural network, train the model on the training set and test it on the test set. 
+    Print the loss average of the loss of the given test set.
+    Inspire from lab10 of the class.
+
+    Args:
+        model : Neural network model.
+        criterion : loss function use in our model.
+        dataset_train (tensor): training set.
+        dataset_test (tensor): test set.
+        optimizer : optimezer used.
+        num_epochs (int): number of iterations.
+        f (function): loss function used to compute the difference between the prediction and the target.
+        device : gpu if available otherwise cpu.
+    """
+    print("Starting training")
+    for epoch in range(num_epochs):
+        # Train an epoch
+        model.train()
+        for batch_x, batch_y in dataset_train:
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+
+            # Evaluate the network (forward pass)
+            prediction = model(batch_x)
+            loss = criterion(prediction, batch_y)
+
+            # Compute the gradient
+            optimizer.zero_grad()
+            loss.backward()
+
+            # Update the parameters of the model with a gradient step
+            optimizer.step()
+
+        # Test the quality on the test set
+        model.eval()
+        accuracies_test = []
+        for batch_x, batch_y in dataset_test:
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+
+            # Evaluate the network (forward pass)
+            prediction = model(batch_x)
+            accuracies_test.append(f(prediction, batch_y))
+
+        if 'MAE' in str(f):
+            print("Epoch {} | Test MAE: {:.5f}".format(epoch, sum(accuracies_test).item()/len(accuracies_test)))
+        elif 'MSE' in str(f):
+            print("Epoch {} | Test MSE: {:.5f}".format(epoch, sum(accuracies_test).item()/len(accuracies_test)))
